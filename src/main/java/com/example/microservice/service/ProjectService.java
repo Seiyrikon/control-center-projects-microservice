3a46a17f7e16d67f14aaa599afe24c238e7a9adb
Ricky Galpo <rickygalpo7777@gmail.com>
2023-10-24 13:27:04 +0800

    minor cleanup

--- src/main/java/com/example/microservice/service/ProjectService.java
+++ src/main/java/com/example/microservice/service/ProjectService.java
@@ -2,10 +2,7 @@ package com.example.microservice.service;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
-
 import com.example.microservice.model.ProjectList;
-import com.example.microservice.shared.Response;
 
 //implemented by ProjectServiceImpl from service/serviceimpl/ProjectServiceImpl.java
 public interface ProjectService {
