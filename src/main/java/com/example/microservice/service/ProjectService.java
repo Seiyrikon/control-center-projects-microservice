f72df5cb390867a1ae16e0e4565966bcc142a94c
Ricky Galpo <rickygalpo7777@gmail.com>
2023-10-24 13:18:33 +0800

    Data from backend are being displayed on frontend

--- src/main/java/com/example/microservice/service/ProjectService.java
+++ src/main/java/com/example/microservice/service/ProjectService.java
@@ -1,10 +1,14 @@
 package com.example.microservice.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
+import com.example.microservice.model.ProjectList;
 import com.example.microservice.shared.Response;
 
 //implemented by ProjectServiceImpl from service/serviceimpl/ProjectServiceImpl.java
 public interface ProjectService {
-    public ResponseEntity<Response> projectList();
+    // public ResponseEntity<Response> projectList();
+    public List<ProjectList> projectList();
 }
--- src/main/java/com/example/microservice/service/serviceimpl/ProjectServiceImpl.java
+++ src/main/java/com/example/microservice/service/serviceimpl/ProjectServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.microservice.service.serviceimpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.microservice.dao.ProjectDao;
+import com.example.microservice.model.ProjectList;
 import com.example.microservice.service.ProjectService;
 import com.example.microservice.shared.Response;
 
@@ -20,21 +23,29 @@ public class ProjectServiceImpl implements ProjectService {
     //Initialization of object for response using the Response class found in shared/Response.java
     Response response = new Response();
 
+    // //Overrides the method of projectList from ProjectService.java found in service/ProjectService.java
+    // @Override
+    // //Used ResponseEntity to leverage the use of ResponseEntity method to properly handle responses sent by this API.
+    // public ResponseEntity<Response> projectList() {
+    //     //Saving the value returned by the method of projectList from ProjectDao to the response object.
+    //     response.setProjectList(projectDao.projectList());
+
+    //     //Checks if the object has value and returns a success response with the value alongside it.
+    //     if(response.getProjectList() != null) {
+    //         return ResponseEntity.ok(response);
+    //     } else {
+    //         //Error handling.
+    //         response.setErrorMessage("Something went wrong. Check your database or spring boot if they are running properly. Try reloading your spring boot app.");
+    //         return ResponseEntity.internalServerError().body(response);
+    //     }
+    // }
+
     //Overrides the method of projectList from ProjectService.java found in service/ProjectService.java
     @Override
     //Used ResponseEntity to leverage the use of ResponseEntity method to properly handle responses sent by this API.
-    public ResponseEntity<Response> projectList() {
-        //Saving the value returned by the method of projectList from ProjectDao to the response object.
-        response.setProjectList(projectDao.projectList());
+    public List<ProjectList> projectList() {
 
-        //Checks if the object has value and returns a success response with the value alongside it.
-        if(response.getProjectList() != null) {
-            return ResponseEntity.ok(response);
-        } else {
-            //Error handling.
-            response.setErrorMessage("Something went wrong. Check your database or spring boot if they are running properly. Try reloading your spring boot app.");
-            return ResponseEntity.internalServerError().body(response);
-        }
+        return projectDao.projectList();
     }
 
     
